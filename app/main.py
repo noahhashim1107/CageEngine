5e488a293e98682b06e9cb3a40e8a0abbb65dc1f
noahhashim1107 <noahhashim1107@gmail.com>
2025-07-28 23:56:16 +0100

    Frontend Integration begun
    Adding CORS middleware, list of allowed origins, allowed all HTTP methods and all headers.
    
    This first step is so that the frontend can eventually call the backend with CORS enabled

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import router 
 from app.logging_config import configure_logging
 
 
 app = FastAPI()
 configure_logging()
+app.include_router(router)
+
+origins = [
+    "http://localhost:3000",
+    "https://example-frontend.com"  #replace with real domain 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
